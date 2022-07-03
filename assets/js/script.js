1a7fe55c511992729e0c98749eda12d2c7a6b19e
MatiasCS1997 <mcorderos@miucsh.cl>
2022-07-02 22:56:34 -0400

    java update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 let btnCalcular = document.querySelector("#get-total");
 btnCalcular.addEventListener("click", () => {
   let inputCantidad = document.querySelector("#cantidad").value;
-  let txtmonto = document.querySelector("#monto");
+  let txtmonto = document.querySelector("#amount");
   txtmonto.innerHTML = inputCantidad;
+  let colors = document.querySelector("#colors").value;
+  document.querySelector("#selectedColor");
+  document.getElementById("coloruser").style.color = colors;
+  /*price*/
+  let selectPrice = document.querySelector("#price").value;
+  /*total*/
+  let txtTotal = document.querySelector("#total");
+  txtTotal.innerHTML = selectPrice * inputCantidad;
 });
